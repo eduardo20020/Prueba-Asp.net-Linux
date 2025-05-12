440b72672a19417f305a5f521beaf3a14d86d6b4
brandon <brandonde2002@gmail.com>
2025-05-11 21:11:19 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LaNueva.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
